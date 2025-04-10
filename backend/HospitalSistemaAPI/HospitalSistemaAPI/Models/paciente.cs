a50a0883481898e4d004d11c4a29d52b36ef5b4a
Carlos Orozco <carlosorozcok@gmail.com>
2025-04-10 05:42:40 -0600

    orden de carpetas y creación de proyecto visual estudio

---
+++ backend/HospitalSistemaAPI/HospitalSistemaAPI/Models/paciente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HospitalSistemaAPI.Models
+{
+    public class Paciente
+    {
+        public required int idPaciente { get; set; }
+        public required string Nombre { get; set; }
+        public required string Genero { get; set; }
+        public required string FechaIngreso { get; set;}
+
+
+
+}
